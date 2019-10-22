c8fcdaf0773e769795d6da4efa034a80b25098c0
weeehabla <weeehaplay@gmail.com>
2019-10-22 09:59:28 +0100

    my answers to wormup 5

--- warmup5.js
+++ warmup5.js
@@ -7,3 +7,90 @@
 //     -calculate the number of male friends that your class have by writing a function called nbOfMale.
 //     -Write a function searchMates that, given a query and an array of Mates,
 //           searches the array of mates for "matching" mate. You will decide what way you want to write your search algorithm.
+
+
+
+var classmates = [ {first : "mohamed", last : "fared " , age : 32 , country :"ibya" , gender:"male" } ,
+{first : "omar", last : "bara" , age : 32 , country :"ibya" , gender:"male" },
+{first : "asma", last : "benali" , age : 20, country :"tunisia" , gender:"female" },
+{first : "mohamed", last : "sbeta" , age : 20, country :"tunisia" , gender:"female" }
+
+];
+function addFriend(first , last , age , country , gender ){
+
+	var mate ={ first : first ,
+		last : last , 
+		age : age , 
+		country : country ,
+		gender : gender
+	}
+classmates.push(mate)
+
+}
+
+function displayFriend(array){
+
+	for ( i = 0 ; i < array.length ; i++)
+	{
+		console.log(array[i])
+	}
+}
+
+function nb0fMale(array){
+ var acc = 0 ;
+  for ( i = 0 ; i < array.length ; i++ ) 
+  	{
+
+  		
+  		if ( array[i].gender === "male"){
+
+  			acc++
+  		}
+
+  	}
+  	return acc
+
+}
+
+function searchMate(array,name){
+
+	var result = [] ;
+
+	for(i = 0 ; i < array.length ; i++)
+
+	{
+
+		if(array[i].first === name ){
+			
+
+			result.push(array[i]);
+		}
+
+
+
+
+	}
+	return result ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
