7e356d1e28eb7e44d30b6d0b67377f6675686510
weeehabla <weeehaplay@gmail.com>
2019-10-25 09:46:52 +0100

    my answer

--- warmUp8.js
+++ warmUp8.js
@@ -2,3 +2,37 @@
 //For example:  array: [ 1, 2, 3, 4 ], sub:[ 2, 3, 4 ] => true
 //              array: [ 2, 2, 2, 3, 4 ], sub:[ 2, 4, 3 ] => true
 //              array: [ 2, 3, 3, "a" ],  sub:[ 3, 3, 3, "a" ] => false
+
+
+function isSUbset(array1,array2){
+
+
+ var count = 0 ;
+
+  for (var i = 0 ; i <array2.length ; i++){
+
+    for (var j = 0 ; j < array1.length ; j++){
+
+      if ( array2[i] === array1[j]){
+           
+           array2.splice(i,1)
+           array1.splice(j,1)
+           j=0; 
+           i = 0 ; 
+
+     
+        }
+
+      }
+      console.log (array2.length)
+    if (array2.length === 0){
+
+      return true
+
+      }
+    else{
+              return false
+      }
+   
+  }
+}
