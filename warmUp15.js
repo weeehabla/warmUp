183b4012d9cc208c8772b7554fca880e9625b19b
weeehabla <weeehaplay@gmail.com>
2019-11-05 09:55:39 +0100

    my answer

--- warmUp15.js
+++ warmUp15.js
@@ -4,3 +4,43 @@
 //
 //
 // Note : you can`t use any built-in functions .
+
+
+//============== almots there but need more time to think about it :))
+
+function reverseStr(string){
+ 
+ var str = "";
+ var str1 = "";
+ var str2 = "";
+ var str3 = "";
+
+    for ( var i = string.length-1 ; i >= 0; i--) {
+
+        str += string[i] 
+    }
+     for( var i = 0 ; i < str.length; i++) {
+
+          str1 += str[i]
+
+        if(str[i] === " "){
+
+             str2 += str1
+
+        if(str2.length === str1.length) {
+
+            for ( var i = str1.length-1 ; i >= 0; i--) {
+
+                     str3 += str1[i] 
+                }
+
+                console.log(str3)
+        }   
+     }
+ }
+ 
+    return str
+
+}
+
+ 		
