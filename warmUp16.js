bafebb6cc1a59e8cad51ff145409468a304638d7
weeehabla <weeehaplay@gmail.com>
2019-11-06 09:52:45 +0100

    still need alot of work

--- warmUp16.js
+++ warmUp16.js
@@ -16,3 +16,51 @@
 // Constraints:
 // 1 <= text.length <= 10^4
 // text consists of lower case English letters only.
+
+
+function balloon(string) {
+
+   var str =  string.split("");
+   var  array = [];
+   var word = '';
+   var countA = 0 ; 
+   var countB = 0 ;
+   var countL = 0 ;
+   var countN = 0 ; 
+   var countO = 0 ;
+
+
+    for ( var i = 0 ; i < str.length ; i++) {
+
+        if( str[i] === "b" || str[i] === "a" || str[i] === "o" || str[i] === "l" || str[i] === "n" ) {
+        array.push(str[i])
+        }
+
+    }
+     array.sort()
+
+     for( var i = 0 ; i < array.length ; i++ ) {
+
+        if (array[i] === "a") {
+            countA += 1;
+        }
+        else if (array[i] === "b") {
+            countB += 1;
+        }
+        else if ( array[i] === "n") {
+            countN += 1 ; 
+        }
+        else if ( array[i] === "l") {
+            countL += 1 ; 
+        }
+        else
+            countO += 1 ; 
+
+     }
+
+     if (countA === 1 && countB === 1 && countL === 2 && countN === 1 && countO === 2 ) {
+        return 1
+     }
+
+    return array 
+}
