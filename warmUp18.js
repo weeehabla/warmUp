c2a308f2d1d3862b40855c419a111b4215cc50ad
weeehabla <weeehaplay@gmail.com>
2019-11-09 09:44:46 +0100

    my answer

--- warmUp18.js
+++ warmUp18.js
@@ -10,3 +10,47 @@
 
 // input   =  "aaaaaaaaaaaa"
 // result  =  "123456789101112"
+
+// =========== The its not working just want to try the concept only the one one of the prolem 
+// =========== to colect the symbol the count of each one 
+
+function countChar(str) {
+
+  var count0 = 0 ;
+  var count1 = 0 ; 
+  var count2 = 0 ; 
+  var count3 = 0 ;
+  var string = '';
+  var array = str.toLowerCase().split("");
+
+  for ( var i = 0 ; i < array.length ; i++) {
+
+    if ( array[i] === "h") {
+
+       count0++
+       string += count0
+       array.splice(i,1)
+      
+       
+    }
+    if ( array[i] === "e") {
+
+       count1++
+       string += count1
+       array.splice(i,1)
+    }
+    if ( array[i] === "l") {
+
+       count2++
+       string += count2
+       array.splice(i,1)
+    }
+    // if ( array[i] === "o") {
+
+    //    count3++
+    //    string += count3
+    //    array.splice(i,1)
+    // }  
+  }
+  return string
+}
